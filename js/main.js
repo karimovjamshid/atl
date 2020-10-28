64abccbb3dec8b453cfad8a6e6559c3e83eb626a
karimovjamshid <67834450+karimovjamshid@users.noreply.github.com>
2020-10-28 18:13:21 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// let pageNum = document.querySelector(".pagination__num");
+
+// pageNum.addEventListener('click', function(event){
+//     event.preventDefault();
+//     pageNum.classList.toggle("pagination__num--active");
+// })
