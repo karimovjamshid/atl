85164f6e1ce2aae3ac44fb2d2a2307f8f38c0674
karimovjamshid <67834450+karimovjamshid@users.noreply.github.com>
2020-10-30 14:11:05 +0500

    Version 2.00
    Tabs have been added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-// let pageNum = document.querySelector(".pagination__num");
+let pageNum = document.querySelectorAll(".pagination__num");
+let pageContents = document.querySelectorAll('.park__content')
 
-// pageNum.addEventListener('click', function(event){
-//     event.preventDefault();
-//     pageNum.classList.toggle("pagination__num--active");
-// })
+pageNum.forEach(function(page){
+    page.addEventListener("click", function(){
+        let currentPage = page;
+        let pageId = currentPage.getAttribute('data-tab');
+        let currentTab = document.querySelector(pageId);
+
+        if(! currentPage.classList.contains('pagination__num--active')){
+            pageNum.forEach(function(page){
+                page.classList.remove('pagination__num--active')
+            })
+            pageContents.forEach(function(page){
+                page.classList.remove('park__content--active')
+            })
+            currentPage.classList.add('pagination__num--active');
+            currentTab.classList.add('park__content--active')
+        }
+
+       
+    })
+})
+document.querySelector(".pagination__num").click();
